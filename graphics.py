854c39ec42c3482df4db8ba37d8cb035d5602872
Muhammad Ahmad <muhammadahmadherl@gmail.com>
2026-03-26 18:17:05 +0500

    Fixed the canvas issues YAY!!!!

--- graphics.py
+++ graphics.py
@@ -8,23 +8,27 @@ class FileManagerApp:
         self.root = tk.Tk()
         self.root.geometry(f"{width}x{height}")
 
+        #Giving weight to the rows and columns of the root window to make the canvas expand to fill the window
         self.root.grid_columnconfigure(0, weight=1)
-        self.root.grid_columnconfigure(1, weight=1)
-        self.root.grid_rowconfigure(0, weight=0)
-        self.root.grid_rowconfigure(1, weight=1)
-        self.root.grid_rowconfigure(2, weight=0)
+        self.root.grid_rowconfigure(0, weight=1)
 
+        #Creating a canvas to hold the title bar and the theme menu, and making it expand to fill the window
         self.canvas = tk.Canvas(self.root, width=width, height=height)
-        self.canvas.grid(column=0, row=0, sticky='nsew', columnspan=2)
+        self.canvas.grid(column=0, row=0, sticky='nsew')
+
+        self.canvas.grid_columnconfigure(0, weight=1)
+        self.canvas.grid_columnconfigure(1, weight=1)
+        self.canvas.grid_columnconfigure(2, weight=1)
 
         self.root.title("Zenth")
+
         # Removing the title bar and borders of the window to create a frameless window
         self.root.overrideredirect(True)
 
         ##################################TEMP#################################
         tk.Button(self.canvas,
                   text="Exit",
-                  command=self.root.destroy).grid(row=3, column=2)
+                  command=self.root.destroy).grid(row=3, column=2, sticky='e', padx=10, pady=10)
         #######################################################################
 
         #Initialize the theme from the theme module
@@ -32,10 +36,11 @@ class FileManagerApp:
 
     def titleBar(self):
         title_bar = tk.Frame(self.canvas, bg="gray", relief='raised', bd=1)
-        title_bar.grid(row=0, column=0, sticky='ew')
+        title_bar.grid_columnconfigure(0, weight=1)
+        title_bar.grid(row=0, column=0, columnspan=3, sticky='ew')
 
         title_label = tk.Label(title_bar, text="Zenth", bg="gray", fg="white")
-        title_label.grid(row=0, column=0)
+        title_label.grid(row=0, column=0, sticky='w', padx=10)
 
 
     def themeMenu(self):
